1defbbcb2aba1a3d17a5bbd66e45907a610b7120
mgabriele1 <55958010+mgabriele1@users.noreply.github.com>
2020-05-21 16:01:27 -0400

    Styling
    fixed some style issues

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let dropDown = document.getElementById('ddcontent');
 const clickMe = document.getElementById ('helpclick')
 
-dropDown.style.dis
-
 clickMe.addEventListener ("click", function () {
     if (dropDown.style.display === "none") {
         dropDown.style.display = "block";
@@ -12,4 +10,31 @@ clickMe.addEventListener ("click", function () {
   },
   false);
 
+let dropDown2 = document.getElementById('ddcontent2');
+const clickMe2 = document.getElementById ('helpclick2')
 
+clickMe2.addEventListener ("click", function () {
+    if (dropDown2.style.display === "none") {
+        dropDown2.style.display = "block";
+      } else {
+        dropDown2.style.display = "none";
+      }
+  },
+  false);
+
+let filter1 = document.getElementById('filter1')
+filter1.addEventListener("click", function() {
+  if (filter1.className != "clicked") {
+    filter1.classList.replace('clicked');
+  }
+  else {
+    filter1.classList.replace('unclicked');
+  }
+},
+false);
+
+
+var slides = document.getElementsByClassName("slide");
+for (var i = 0; i < slides.length; i++) {
+   Distribute(slides.item(i));
+}
